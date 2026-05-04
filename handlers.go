082bbe0dc848f96f9f8609379713429a0951a7f4
Kyle Robinson <krobin37@byu.edu>
2026-05-04 11:56:42 -0600

    Updated the login handler function to verify the username and password, then throw an error if either option is invalid

--- handlers.go
+++ handlers.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type UserFile struct {
+	Users []User `json:"users"`
+}
 
 func home(w http.ResponseWriter, r *http.Request) {
 	files := []string{
@@ -24,6 +37,8 @@ func home(w http.ResponseWriter, r *http.Request) {
 		log.Print(err.Error())
 		return
 	}
+
+	log.Print("Home page served")
 }
 
 func loginPage(w http.ResponseWriter, r *http.Request) {
@@ -52,9 +67,58 @@ func loginPage(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	log.Print("Login page served")
+
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
 	// TODO: Finish implementing login process
 	log.Print("Login POST request received. Process is not yet implemented.")
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	log.Printf("Login attempt for user: %s. Used password: %s", username, password)
+
+	userFile, err := os.ReadFile("users.json")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Print("Read users.json successfully")
+
+	log.Printf("Current contents of users.json: %s", string(userFile))
+
+	var users UserFile
+	err = json.Unmarshal(userFile, &users)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	log.Print("Unmarshaled users successfully")
+
+	log.Print("Checking credentials against users")
+	for _, user := range users.Users {
+		if user.Username == username {
+			// Login successful
+			err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+			if err != nil {
+				log.Printf("Failed login attempt for user %s: %v", user.Username, err)
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+			log.Printf("Successfully logged in user %s", user.Username)
+			return
+		}
+	}
+
+	log.Printf("User %s does not exist", username)
+	http.Error(w, "Unauthorized", http.StatusUnauthorized)
 }
