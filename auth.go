5fc1d9feeef940f16e061b9a68634e3b7ff63695
Kyle Robinson <krobin37@byu.edu>
2026-05-04 13:00:16 -0600

    Added declarations for methods to manage sessions and methods to make sure a user is logged in or has the right role

--- auth.go
+++ auth.go
@@ -1,6 +1,12 @@
 package main
 
-import "time"
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"net/http"
+	"sync"
+	"time"
+)
 
 type Session struct {
 	Username  string
@@ -9,3 +15,23 @@ type Session struct {
 }
 
 var sessions = make(map[string]Session)
+var sessionsMu sync.RWMutex
+
+func newSessionID() (string, error) {
+	b := make([]byte, 32)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(b), nil
+}
+
+func createSession(username, role string) (string, error) { return "", nil }
+
+func getSession(r *http.Request) (*Session, error) { return nil, nil }
+
+func clearSession(w http.ResponseWriter, r *http.Request) error { return nil }
+
+func requireAuth(next http.HandlerFunc) http.HandlerFunc { return nil }
+
+func requireRole(role string, next http.HandlerFunc) http.HandlerFunc { return nil }
