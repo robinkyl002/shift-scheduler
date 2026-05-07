b44d0fd7b1236c99d25f7c8d4192e75417065216
Kyle Robinson <krobin37@byu.edu>
2026-05-07 11:15:09 -0600

    Added file with stubs for helper functions to be used by submitSchedule

---
+++ cmd/webserver/schedule-service.go
@@ -0,0 +1,17 @@
+package main
+
+func loadSchedules() (ScheduleFile, error) {
+	return ScheduleFile{}, nil
+}
+
+func saveSchedules(schedules ScheduleFile) error {
+	return nil
+}
+
+func findScheduleIndex(username string) int {
+	return 0
+}
+
+func upsertSchedule(submission ScheduleSubmission) error {
+	return nil
+}
