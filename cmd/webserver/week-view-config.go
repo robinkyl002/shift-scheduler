df29a97bb13c2590abbe7133d5fd23766616fd4a
Kyle Robinson <krobin37@byu.edu>
2026-05-07 13:36:40 -0600

    Created week-view-config.go for constant schedule grid configurations

---
+++ cmd/webserver/week-view-config.go
@@ -0,0 +1,8 @@
+package main
+
+var weekDays = []string{"Mon", "Tue", "Wed", "Thu", "Fri"}
+var weekHours = []int{8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
+
+const slicesPerHour = 6
+const minutesPerSlice = 10
+const scheduleStartHour = 8
